48f7ff2deab546337c16e6c1cd7e90224c8ce8b3
Randall Degges <r@rdegges.com>
2014-07-01 14:46:40 -0700

    Fixing controllers.

--- controllers.js
+++ controllers.js
@@ -1,8 +1,7 @@
 var forms = require('./forms');
-var stormpath = require('./index');
 
 
-module.exports.login = function(req, res, next) {
+module.exports.login = function(req, res) {
   req.app.set('views', __dirname + '/views');
   req.app.set('view engine', 'jade');
   res.locals.csrfToken = req.csrfToken();
@@ -11,23 +10,20 @@ module.exports.login = function(req, res, next) {
     // If we get here, it means the user is submitting a login request, so we
     // should attempt to log the user into their account.
     success: function(form) {
-      stormpath.application.authenticateAccount({
+      req.app.get('stormpathApplication').authenticateAccount({
         username: form.data.login,
         password: form.data.password
       }, function(err, result) {
         if (err) {
           res.render('login', { error: err.userMessage, form: form });
-          next();
         } else {
           result.getAccount(function(err, account) {
             if (err) {
               res.render('login', { error: err.userMessage, form: form });
-              next();
             } else {
               req.session.user = account;
               res.locals.user = account;
               res.redirect(302, req.app.get('stormpathRedirectUrl'));
-              next();
             }
           });
         }
@@ -37,24 +33,21 @@ module.exports.login = function(req, res, next) {
     // If we get here, it means the user didn't supply required form fields.
     error: function(form) {
       res.render('login', { error: 'Required fields missing.', form: form });
-      next();
     },
 
     // If we get here, it means the user is doing a simple GET request, so we
     // should just render the login template.
     empty: function(form) {
       res.render('login', { form: form });
-      next();
     }
   });
 };
 
 
-module.exports.logout = function(req, res, next) {
+module.exports.logout = function(req, res) {
   if (req.session) {
     req.session.reset();
   }
 
   res.redirect('/');
-  next();
 };
