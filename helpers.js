dd033244953ad8576275466d672a6e262fb72d1a
Randall Degges <r@rdegges.com>
2014-06-30 17:28:21 -0700

    Moving settings logic to helpers.

--- helpers.js
+++ helpers.js
@@ -3,7 +3,7 @@ var stormpath = require('stormpath');
 var sp = require('./index');
 
 
-module.exports.getUser = function(req, res) {
+module.exports.getUser = function(req, res, callback) {
   if (req.session && req.session.user) {
     sp.client.getAccount(req.session.user.href, { expand: 'customData' }, function(err, account) {
       if (err) {
@@ -12,6 +12,45 @@ module.exports.getUser = function(req, res) {
         req.session.user = account;
         res.locals.user = account;
       }
+      callback();
     });
   }
-}
+};
+
+
+module.exports.initSettings = function(app, opts) {
+  return function(next) {
+    app.set('stormpathSecretKey', opts.secretKey || process.env.STORMPATH_SECRET_KEY);
+    app.set('stormpathEnableHttps', opts.enableHttps || process.env.STORMPATH_ENABLE_HTTPS || false);
+    app.set('stormpathSessionDuration', opts.sessionDuration || process.env.STORMPATH_SESSION_DURATION || null);
+    app.set('stormpathApiKeyId', opts.apiKeyId || process.env.STORMPATH_API_KEY_ID);
+    app.set('stormpathApiKeySecret', opts.apiKeySecret || process.env.STORMPATH_API_KEY_SECRET);
+    app.set('stormpathApiKeyFile', opts.apiKeyFile || process.env.STORMPATH_API_KEY_FILE);
+    app.set('stormpathApplication', opts.application || process.env.STORMPATH_APPLICATION);
+    app.set('stormpathRedirectUrl', opts.redirectUrl || process.env.STORMPATH_REDIRECT_URL || '/');
+
+    next();
+  };
+};
+
+
+module.exports.checkSettings = function(app) {
+  return function(next) {
+    if (!app.get('stormpathSecretKey')) {
+      throw new Error('ERROR: No Stormpath secretKey specified.');
+    }
+
+    if (!(
+      (app.get('stormpathApiKeyID') && app.get('stormpathApiKeySecret')) ||
+      app.get('stormpathApiKeyFile')
+    )) {
+      throw new Error('ERROR: No Stormpath credentials specified.');
+    }
+
+    if (!app.get('stormpathApplication')) {
+      throw new Error('ERROR: No Stormpath application specified.');
+    }
+
+    next();
+  };
+};
