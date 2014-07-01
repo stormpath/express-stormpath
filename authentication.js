e475e7bd0ea2e00553c4d9c7d10e63787f99b975
Randall Degges <r@rdegges.com>
2014-07-01 14:45:24 -0700

    Fixing our authentication redirection middleware.

--- authentication.js
+++ authentication.js
@@ -3,7 +3,8 @@ var stormpath = require('./index');
 
 module.exports.loginRequired = function(req, res, next) {
   if (!res.locals.user) {
-    res.redirect(302, '/login');
+    var url = req.app.get('stormpathLoginUrl') + '?next=' + encodeURIComponent(req.route.path);
+    res.redirect(302, url);
   } else {
     next();
   }
