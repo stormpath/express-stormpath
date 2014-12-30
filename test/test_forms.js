1e106b94c8e0ca5ec7dbf05c064cacc8e8896372
Randall Degges <r@rdegges.com>
2014-12-30 15:27:31 -0800

    Adding tests for registrationForm.

---
+++ test/test_forms.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var assert = require('assert');
+
+var forms = require('../lib/forms.js');
+
+describe('registrationForm', function() {
+  it('should require an email address', function() {
+    var form = forms.registrationForm.bind({
+      username: 'rdegges',
+      givenName: 'Randall',
+      surname: 'Degges',
+      password: 'blahblahBLAH!1234'
+    });
+
+    form.validate(function(err) {
+      assert.equal(err, 'Email is required.');
+    });
+  });
+
+  it('should require a password', function() {
+    var form = forms.registrationForm.bind({
+      username: 'rdegges',
+      givenName: 'Randall',
+      surname: 'Degges',
+      email: 'randall@stormpath.com'
+    });
+
+    form.validate(function(err) {
+      assert.equal(err, 'Password is required.');
+    });
+  });
+});
