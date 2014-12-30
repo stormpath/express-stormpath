058b29e6aee320ce234951fdd6c796383d706417
Randall Degges <r@rdegges.com>
2014-12-30 15:29:08 -0800

    Adding tests for forms.loginForm.

--- test/test_forms.js
+++ test/test_forms.js
@@ -31,3 +31,25 @@ describe('registrationForm', function() {
     });
   });
 });
+
+describe('loginForm', function() {
+  it('should require a login', function() {
+    var form = forms.loginForm.bind({
+      password: 'blahblahBLAH!1234'
+    });
+
+    form.validate(function(err) {
+      assert.equal(err, 'Login is required.');
+    });
+  });
+
+  it('should require a password', function() {
+    var form = forms.loginForm.bind({
+      login: 'randall@stormpath.com'
+    });
+
+    form.validate(function(err) {
+      assert.equal(err, 'Password is required.');
+    });
+  });
+});
