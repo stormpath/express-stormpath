e48a1e7cd74629fa84fd58da4252726a3682c3f1
Robert <robert@stormpath.com>
2015-06-23 15:06:40 -0700

    tests for the forgot password form

---
+++ test/test_forgot_password.js
@@ -0,0 +1,156 @@
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var async = require('async');
+
+var cheerio = require('cheerio');
+
+var express = require('express');
+
+var request = require('supertest');
+var uuid = require('uuid');
+
+var stormpath = require('../index');
+var stormpathRaw = require('stormpath');
+
+describe('forgotPassword', function() {
+  var stormpathApplication;
+  var stormpathClient = new stormpathRaw.Client();
+
+
+  before(function(done) {
+
+    var appData = { name: uuid.v4() };
+    var opts = { createDirectory: true };
+
+    stormpathClient.createApplication(appData, opts, function(err, app) {
+      if (err) {
+        return done(err);
+      }
+
+      stormpathApplication = app;
+      done();
+    });
+  });
+
+
+
+  after(function(done) {
+    stormpathApplication.delete(done);
+  });
+
+  it('should bind to /forgot if enabled', function(done) {
+    var app = express();
+
+    app.use(stormpath.init(app, {
+      client:{
+        apiKey:{
+          id: process.env.STORMPATH_API_KEY_ID,
+          secret: process.env.STORMPATH_API_KEY_SECRET,
+        },
+      },
+      application: {
+        href: stormpathApplication.href,
+      },
+      web: {
+        forgotPassword:{
+          enabled: true
+        }
+      }
+    }));
+
+    var config = app.get('stormpathConfig');
+
+    request(app)
+      .get('/forgot')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+
+        var $ = cheerio.load(res.text);
+
+        // assert that the form was rendered
+        assert.equal($('form[action="'+config.web.forgotPassword.uri+'"]').length,1);
+
+        done();
+      });
+  });
+
+  it('should return an error if the posted email is not an email', function(done) {
+    var app = express();
+
+    app.use(stormpath.init(app, {
+      client:{
+        apiKey:{
+          id: process.env.STORMPATH_API_KEY_ID,
+          secret: process.env.STORMPATH_API_KEY_SECRET,
+        },
+      },
+      application: {
+        href: stormpathApplication.href,
+      },
+      web: {
+        forgotPassword:{
+          enabled: true
+        }
+      }
+    }));
+
+    setTimeout(function() {
+      request(app)
+        .post('/forgot')
+        .type('form')
+        .send({
+          email: 'not a real email'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            return done(err);
+          }
+          assert(/Please enter a valid email address/.test(res.text));
+          done();
+        });
+    }, 5000);
+  });
+
+  it('should redirect to the next uri if an email is given', function(done) {
+    var app = express();
+
+    app.use(stormpath.init(app, {
+      client:{
+        apiKey:{
+          id: process.env.STORMPATH_API_KEY_ID,
+          secret: process.env.STORMPATH_API_KEY_SECRET,
+        },
+      },
+      application: {
+        href: stormpathApplication.href,
+      },
+      web: {
+        forgotPassword:{
+          enabled: true
+        }
+      }
+    }));
+
+    var config = app.get('stormpathConfig');
+
+    setTimeout(function() {
+      request(app)
+        .post('/forgot')
+        .type('form')
+        .send({
+          email: uuid.v4() + '@stormpath.com',
+        })
+        .expect('Location',config.web.forgotPassword.nextUri)
+        .expect(302,done);
+
+    }, 5000);
+  });
+
+});
