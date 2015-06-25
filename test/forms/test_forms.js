c800a40ebbd9ec845ae31c4486b058e9707d25f8
Randall Degges <r@rdegges.com>
2015-06-25 12:49:01 -0700

    Moving registration form tests into their own file.

--- test/forms/test_forms.js
+++ test/forms/test_forms.js
@@ -4,34 +4,6 @@ var assert = require('assert');
 
 var forms = require('../lib/forms');
 
-describe('registrationForm', function() {
-  it('should require an email address', function() {
-    var form = forms.registrationForm.bind({
-      username: 'rdegges',
-      givenName: 'Randall',
-      surname: 'Degges',
-      password: 'blahblahBLAH!1234'
-    });
-
-    form.validate(function(err) {
-      assert.equal(err, 'Email is required.');
-    });
-  });
-
-  it('should require a password', function() {
-    var form = forms.registrationForm.bind({
-      username: 'rdegges',
-      givenName: 'Randall',
-      surname: 'Degges',
-      email: 'randall@stormpath.com'
-    });
-
-    form.validate(function(err) {
-      assert.equal(err, 'Password is required.');
-    });
-  });
-});
-
 describe('loginForm', function() {
   it('should require a login', function() {
     var form = forms.loginForm.bind({
