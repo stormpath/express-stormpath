8e4e80633c836488b5561be35db4695a42f8305a
Randall Degges <r@rdegges.com>
2015-06-25 12:50:43 -0700

    Moving forgot password form tests to its own place.

--- test/forms/test_forms.js
+++ test/forms/test_forms.js
@@ -4,16 +4,6 @@ var assert = require('assert');
 
 var forms = require('../lib/forms');
 
-describe('forgotPasswordForm', function() {
-  it('should require an email', function() {
-    var form = forms.forgotPasswordForm.bind({});
-
-    form.validate(function(err) {
-      assert.equal(err, 'Email is required.');
-    });
-  });
-});
-
 describe('changePasswordForm', function() {
   it('should require a password', function() {
     var form = forms.changePasswordForm.bind({
