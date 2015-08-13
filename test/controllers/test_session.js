de7619cbbf2b06f44d2e23998b6ac89e8aaf2612
Robert <robert@stormpath.com>
2015-08-12 18:24:39 -0700

    attempt to use refresh token to get a new access token

---
+++ test/controllers/test_session.js
@@ -0,0 +1,6 @@
+'use strict';
+
+describe('access token session',function() {
+  it.skip('should be refresh-able using the refresh token');
+  it.skip('should fail authentication if the access token and refresh token are expired');
+});
