13c587a7647a1effd87f5b7af45bcf3d7945bc5a
Randall Degges <r@rdegges.com>
2015-08-18 17:06:21 -0700

    Adding tests for helpers.validateAccount.
    This validation helper essentially validates that all required fields are
    present before allowing an account to be created.  NOTE: Accounts will still be
    eventually created / validated on the Stormpath side as well, but this allows
    for us to allow users to specify their own additional validation options! <3

--- test/test_helpers.js
+++ test/test_helpers.js
@@ -54,3 +54,69 @@ describe('getRequiredRegistrationFields', function() {
     });
   });
 });
+
+describe('validateAccount', function() {
+  var config = {
+    web: {
+      register: {
+        fields: {
+          givenName: {
+            name: 'givenName',
+            required: true
+          },
+          surname: {
+            name: 'surname',
+            required: true
+          },
+          email: {
+            name: 'email',
+            required: true
+          },
+          password: {
+            name: 'password',
+            required: true
+          }
+        }
+      }
+    }
+  };
+
+  it('should return null if no errors are present', function(done) {
+    var accountData = {
+      givenName: 'Randall',
+      surname: 'Degges',
+      email: 'randall@stormpath.com',
+      password: 'FASRbaBjkrqJSNVlUrV2ZyUy5iUX8UEZ3TW3nejX'
+    };
+
+    helpers.validateAccount(accountData, config, function(errors) {
+      assert.equal(errors, null);
+      done();
+    });
+  });
+
+  it('should return errors if errors are present', function(done) {
+    var accountData = {
+      givenName: 'Randall',
+      surname: 'Degges',
+      email: 'randall@stormpath.com'
+    };
+
+    helpers.validateAccount(accountData, config, function(errors) {
+      assert.equal(errors.length, 1);
+      done();
+    });
+  });
+
+  it('should return the right number of errors if errors are present', function(done) {
+    var accountData = {
+      givenName: 'Randall',
+      surname: 'Degges'
+    };
+
+    helpers.validateAccount(accountData, config, function(errors) {
+      assert.equal(errors.length, 2);
+      done();
+    });
+  });
+});
