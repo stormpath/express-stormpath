e6f7ce573647d3a58650838d844d9ac9225aad57
Randall Degges <r@rdegges.com>
2015-09-23 16:05:07 -0700

    Updating tests to ensure passwordConfirm data is not supplied.

--- test/test_helpers.js
+++ test/test_helpers.js
@@ -189,15 +189,50 @@ describe('sanitizeFormData', function() {
 });
 
 describe('prepAccountData', function() {
+  var config = {
+    web: {
+      register: {
+        fields: {
+          givenName: {
+            name: 'givenName',
+            required: true
+          },
+          surname: {
+            name: 'surname',
+            required: true
+          },
+          email: {
+            name: 'email',
+            required: true
+          },
+          password: {
+            name: 'password',
+            required: true
+          },
+          passwordConfirm: {
+            name: 'passwordConfirmWoo',
+            required: true
+          }
+        }
+      }
+    }
+  };
+
   it('should throw an error if formData is not supplied', function() {
     assert.throws(function() {
       helpers.prepAccountData();
     }, Error);
   });
 
-  it('should throw an error if callback is not supplied', function() {
+  it('should throw an error if stormpathConfig is not supplied', function() {
     assert.throws(function() {
       helpers.prepAccountData({});
+    }, Error);
+  });
+
+  it('should throw an error if callback is not supplied', function() {
+    assert.throws(function() {
+      helpers.prepAccountData({}, config);
     }, Error);
   });
 
@@ -207,8 +242,11 @@ describe('prepAccountData', function() {
       surname: 'Degges',
       email: 'r@rdegges.com',
       password: 'woot!!!!omgHAX',
+      passwordConfirmWoo: 'woot!!!!omgHAX',
       extraData: 'hithere'
-    }, function(accountData) {
+    }, config, function(accountData) {
+      assert.equal(accountData.passwordConfirmWoo, undefined);
+      assert.equal(accountData.customData.passwordConfirmWoo, undefined);
       assert.equal(accountData.extraData, undefined);
       done();
     });
@@ -220,8 +258,9 @@ describe('prepAccountData', function() {
       surname: 'Degges',
       email: 'r@rdegges.com',
       password: 'woot!!!!omgHAX',
+      passwordConfirmWoo: 'woot!!!!omgHAX',
       extraData: 'hithere'
-    }, function(accountData) {
+    }, config, function(accountData) {
       assert.deepEqual(accountData, {
         givenName: 'Randall',
         surname: 'Degges',
