7b452a5dfa0cb89bec104296669adfd85bb917aa
Randall Degges <r@rdegges.com>
2015-09-24 10:55:32 -0700

    Adding a doc building script.

---
+++ build-docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script builds *every* release of the project documentation. This is used
+# to rebuild all of our previous Express project docs. Should probably not be
+# ran unless we update our Sphinx templates, and want to re-generate things all
+# at once.
+
+for tag in `git tag -l`; do
+    git checkout $tag;
+    cd docs;
+    make html && cp -r _build/html ~/Desktop/$tag;
+    cd ..;
+done
