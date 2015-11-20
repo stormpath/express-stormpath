42e88132986b08767c4818c1e8b7f43cd552a779
Robert <robert@stormpath.com>
2015-11-19 16:24:38 -0800

    document the spa root fixture

--- test/fixtures/spa-root-fixture.js
+++ test/fixtures/spa-root-fixture.js
@@ -6,6 +6,17 @@ var fs = require('fs');
 var helpers = require('../helpers');
 var uuid = require('uuid');
 
+/**
+ * This SPA Root Fixture takes in your desired Stormpath Config, and then
+ * attaches the spaRoot option to the web config.  It creates a SPA root file
+ * in the before hook, and cleans it up in the after hook.
+ *
+ * It also provides an assertResponse() function which you can pass to the
+ * end() method of a supertest agent.  It will assert that the SPA root was
+ * served.
+ *
+ * @param {object} stormpathConfig The config for stormpath.init()
+ */
 function SpaRootFixture(stormpathConfig) {
   this.filename = '/tmp/' + uuid.v4();
 
