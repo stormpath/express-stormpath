29f05d95c266c4870e8859c64dc97fe6af1a72ff
Robert <robert@stormpath.com>
2016-02-18 22:49:12 -0800

    fix typos

--- test/fixtures/produces-fixture.js
+++ test/fixtures/produces-fixture.js
@@ -8,8 +8,8 @@ var helpers = require('../helpers');
 
 /**
  * Test fixture for testing the web.produces option.  It requres a Stormpath
- * application an a definition of what content types should be reproduce by
- * the framework instance.
+ * application an a definition of what content types should be produced by the
+ * framework instance.
  *
  * At the moment it is hard-coded to to it's tests against the /login endpoint,
  * that can be changed if we want to test all the endpoints that have content
