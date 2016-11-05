4194c40503307b3f01243ffc949fa61d496bccc1
Robert <robert@stormpath.com>
2016-11-05 13:39:25 -0700

    fix description of fixture

--- test/fixtures/subdomain-multi-tenancy.js
+++ test/fixtures/subdomain-multi-tenancy.js
@@ -6,17 +6,9 @@ var async = require('async');
 var uuid = require('uuid');
 
 /**
- * This fixture creates an Express application which has express-stormpath
- * integrated. It does not pass any special options to the express-stormpath
- * init function. This fixture is meant to represent the pure default state of
- * the library when it does not receive any special configuration.
- *
- * The only parameter that this fixture constructor takes is a Stormpath
- * application reference, as we need to know which Stormpath application to use
- * for the initialization of the library. It is assumed that API Keys for
- * Stormpath are already in the environment.
- *
- * @param {object} stormpathApplication
+ * This fixture creates a Stormpath application that has an organization mapped
+ * to it.  It also creates an account in that organization, for testing
+ * authenticatio against the organization.
  */
 function SubdomainMultiTenancyFixture() {
   this.client = helpers.createClient();
