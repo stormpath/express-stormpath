9d267a8a61375323215cac612201798a6b7786b3
Robert Damphousse <robert.damphousse.devex@okta.com>
2017-07-21 14:56:13 -0700

    Fixes for test data script

--- util/okta-test-data.js
+++ util/okta-test-data.js
@@ -248,7 +248,7 @@ function createApplication(client, next) {
     if (err) {
       return next(err);
     }
-    client.getApplication(oAuthClient.client_id, next);
+    client.getApplication('apps/' + oAuthClient.client_id, next);
   });
 }
 
